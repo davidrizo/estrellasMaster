4dcba5f33e38b6c10213571decbd7668e997920a
David Rizo Valero <drizovalero@gmail.com>
2017-11-27 21:10:21 +0100

    Máquina de estados a la carta estelar. Notificaciones de selección y edición desde la carta estelar a través del modelo.

---
+++ src/es/ua/dlsi/mpaee/estrellas/eventos/EventoTeclado.java
@@ -0,0 +1,16 @@
+package es.ua.dlsi.mpaee.estrellas.eventos;
+
+import es.ua.dlsi.mpaee.estrellas.Evento;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
+
+public class EventoTeclado extends Evento<Text> {
+    KeyEvent eventoTeclado;
+    public EventoTeclado(KeyEvent event) {
+        this.eventoTeclado = event;
+    }
+
+    public KeyEvent getEventoTeclado() {
+        return eventoTeclado;
+    }
+}
